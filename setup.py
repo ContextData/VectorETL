d27c6ff8eaa9b3fba60feb9402228b69f0de1ed8
Babajide Ogunjobi <jogunjobi@gmail.com>
2024-08-05 15:32:14 -0400

    updated pyproject

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="vector-etl",
-    version="0.1.3",
+    version="0.1.4",
     author="Jide Ogunjobi",
     author_email="jide@contextdata.ai",
     description="Lightweight ETL pipeline for processing data into vector databases",
--- vector_etl/__init__.py
+++ vector_etl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .orchestrator import run_etl_process
 
