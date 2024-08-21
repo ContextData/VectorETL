398ed8e3615e4203e42ccfca0fbd97126066881d
Babajide Ogunjobi <jogunjobi@gmail.com>
2024-08-21 16:21:17 -0500

    updated setup and pyproject

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="vector-etl",
-    version="0.1.6.3",
+    version="0.1.6.4",
     author="Jide Ogunjobi",
     author_email="jide@contextdata.ai",
     description="Lightweight ETL pipeline for processing data into vector databases",
@@ -68,6 +68,7 @@ setup(
         "python-magic",
         "pytest",
         "nltk",
+        "pymilvus",
     ],
     entry_points={
         "console_scripts": [
